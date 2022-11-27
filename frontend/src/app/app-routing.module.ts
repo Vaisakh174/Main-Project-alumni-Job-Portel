a96a6a319005d938a86763abbb6e81b3bc5bf6b7
Vaisakh174 <101011734+Vaisakh174@users.noreply.github.com>
2022-11-27 23:43:34 +0530

    admin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './adminComponents/home/home.component';
+import { LoginComponent } from './adminComponents/login/login.component';
 
-const routes: Routes = [{path:'',component:LoginComponent}];
+const routes: Routes = [{path:'',component:LoginComponent},
+{path:"home",component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './adminComponents/login/login.component';
 import { ApiService } from './services/api.service';
 import { AuthService } from './services/auth.service';
 import { TokenInterceptorService } from './services/token-interceptor.service';
+import { HomeComponent } from './adminComponents/home/home.component';
 
 
 
@@ -16,6 +17,7 @@ import { TokenInterceptorService } from './services/token-interceptor.service';
   declarations: [
     AppComponent,
     LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
