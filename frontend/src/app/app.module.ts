94c6000c74d7caed7af15330b625a18b6d5af179
Vaisakh174 <101011734+Vaisakh174@users.noreply.github.com>
2022-11-29 07:55:28 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,8 @@
-import {  HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiService } from './adminModule/services/api.service';
-import { AuthService } from './adminModule/services/auth.service';
-import { TokenInterceptorService } from './adminModule/services/token-interceptor.service';
 import { AdminModule } from './adminModule/admin.module';
 
 
@@ -22,12 +18,7 @@ import { AdminModule } from './adminModule/admin.module';
     BrowserModule,AdminModule,
     AppRoutingModule
   ],
-  providers: [ApiService,AuthService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass:TokenInterceptorService ,
-      multi:true
-    }
+  providers: [
   
     ],
   bootstrap: [AppComponent]
