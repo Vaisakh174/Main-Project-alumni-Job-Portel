c10200716427732328bf7585a6ee1049a275bb85
Vaisakh174 <101011734+Vaisakh174@users.noreply.github.com>
2022-12-12 22:11:45 +0530

    vaisakh update

--- frontend/src/app/alumni/alumniapi.service.ts
+++ frontend/src/app/alumni/alumniapi.service.ts
@@ -9,6 +9,7 @@ export class AlumniapiService {
   constructor(private http:HttpClient) { }
 
   url = "http://localhost:3000";
+  // url=""
 //for new alumni
 postalumni(data: any) {
   return this.http.post(`${this.url}/api/alumni/new`, data);
